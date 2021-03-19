c1d9d12d355a4a01ecd12e4c64dcce1212d95e9d
baloxegal <baloxegal@gmail.com>
2021-03-19 18:12:43 +0200

    Add project files.

---
+++ Test_Writing_Test_TDD/UnitTest1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+//using NUnit.Framework;
+using Moq
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test_Writing_Test_TDD
+{
+    using Writing_Test_TDD;
+    [TestClass]
+    //[TestFixture] - it is denotes this class is for test
+    public class UnitTest1
+    {
+
+        //[SetUp] - it is for intialization data for test
+        //public void SetUp()
+        //{
+        //}
+
+        static object[] AvgData =
+        {
+            new object[]{100, 200, 300},
+            new object[]{1000, 800, 500},
+        };
+
+        [TestMethod]
+        //[TestCase]
+        //[Test]- it is denotes this method is for test
+        public void TestMin()
+        {
+            //Arrange
+            int min;
+            //Act
+            min = Program.Min(3, 4, 5);
+            //Assert
+            Assert.AreEqual(3, min);
+        }
+        [TestMethod]
+        //[TestMethod(2,12,10)] TestCaseAttributes
+        //[TestMethod(6,18,22)]
+        //[TestMethod(2,12,10)] TestCaseSourceAttributes
+        public void TestAvg()
+        {
+            float avg;
+            avg = Program.Avg(10, 3, 6);
+            Assert.AreEqual(5, avg);
+        }
+
+        //MOQ
+        [TestMethod]
+        public void ItShouldCallCreateProduct()
+        {
+
+            //arrange
+            var mockDataAccess = new Mock<IReposio>();
+            mockDataAccess.Setup(m = > m.CreateProduct(It.IsAny<Product>)).Returns(true);
+            var productBusiness = new ProductBusiness(mockDataAccess.Object);
+
+            //act
+            productBusiness.CreateProduct(new Product());
+
+            //assert
+            mockDataAccess.Verify(m = > m.CreateProduct(It.IsAny<Product>), Times.Once())
+ }
+
+
+        //[TearDown] - it is for execute last, after a test
+        //public void TearDown()
+        //{
+        //}
+    }
+}
---
+++ Writing_Test_TDD/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Writing_Test_TDD
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+        public static int Min(int a, int b, int c)
+        {
+            int min = a;
+            if (min > b) min = b;
+            if (min > c) min = c;
+            return min;
+        }
+        public static float Avg(int a, int b, int c)
+        {
+            float avg = ((float)a + b + c) / 3;
+            return avg;
+        }
+    }
+
+    public class ProductBusiness
+    {
+        private readonly IProductDataAccess _productDataAccess;
+
+        public ProductBusiness(IProductDataAccess productDataAccess)
+        {
+            _productDataAccess = productDataAccess;
+        }
+
+        public bool CreateProduct(Product newProduct)
+        {
+            bool result = _productDataAccess.CreateProduct(newProduct);
+            return result;
+        }
+    }
+
+}
